2b9ebd2fd67f572499feb12fe343608da7ecbbb6
João Luiz Schlickmann Kestering <jlsk25@gmail.com>
2018-05-15 15:56:45 -0300

    Navigation
    Add the types of navigation that are common in ionic navigation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,13 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { OtherPage } from '../pages/otherPage/otherPage';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    OtherPage
   ],
   imports: [
     BrowserModule,
@@ -19,7 +21,8 @@ import { HomePage } from '../pages/home/home';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    OtherPage
   ],
   providers: [
     StatusBar,
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { OtherPage } from '../otherPage/otherPage';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,16 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
+  public myString: string = 'Example 1 - This is just a string';
+  public myJSON: any = {text: ''};
+  otherPage: any = OtherPage;
+
   constructor(public navCtrl: NavController) {
 
+  }
+
+  gotoOtherPage() {
+    this.navCtrl.push(OtherPage, {text: 'Example 3 - This is an object'});
   }
 
 }
